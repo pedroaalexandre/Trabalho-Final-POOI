ae522f5623144091942d7662cf6b10c5c6b1fcb1
pedro.aalexandre <pedro.aalexandre@outlook.com>
2025-02-05 21:37:09 -0300

    Criando o contrutor de Produto

--- Produto.java
+++ Produto.java
@@ -3,5 +3,12 @@ public class Produto {
     private int codigo;
     private String nome;
     private double preco;
+    public Produto(int codigo, String nome, double preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    
     
 }
