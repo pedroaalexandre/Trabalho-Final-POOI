40c74d888f2b93ab3c4ea7fb48ebad765c4be2fb
pedro.aalexandre <pedro.aalexandre@outlook.com>
2025-02-05 21:39:28 -0300

    Alterando a classe Produto

--- Produto.java
+++ Produto.java
@@ -3,13 +3,6 @@ public class Produto {
     private int codigo;
     private String nome;
     private double preco;
-    public Produto(int codigo, String nome, double preco) {
-        this.codigo = codigo;
-        this.nome = nome;
-        this.preco = preco;
-    }
-
-    
     
     public Produto(int codigo, String nome, double preco) {
         this.codigo = codigo;
