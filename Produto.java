feb63c03a31723662d97ccd4dba34bb88d1bfe05
Felipe Dornelas <felipegabriel123dornelas@gmail.com>
2025-02-05 21:36:23 -0300

    Adicionando construtor na classe Produto

--- Produto.java
+++ Produto.java
@@ -4,4 +4,10 @@ public class Produto {
     private String nome;
     private double preco;
     
+    public Produto(int codigo, String nome, double preco) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+    }
+    
 }
