fc08f1e9767367703d36f5b6de2918ee30952665
pedro.aalexandre <pedro.aalexandre@outlook.com>
2025-02-05 22:35:14 -0300

    Criando o atributo desconto na classe Cliente e definindo o contrutor sobrecarregado.

--- Cliente.java
+++ Cliente.java
@@ -4,12 +4,20 @@ public class Cliente {
 
     private int codigo;
     private String nome;
+    private double desconto = 0.10;
     private Pedido pedido;
     private List <Produto> produtos;
 
     public Cliente(int codigo, String nome, Pedido pedido) {
         this.codigo = codigo;
         this.nome = nome;
+        this.pedido = pedido;
+    }
+
+    public Cliente(int codigo, String nome, double desconto, Pedido pedido) {       //Construtor sobrecarregado
+        this.codigo = codigo;
+        this.nome = nome;
+        this.desconto = desconto;
         this.pedido = pedido;
     }
 
